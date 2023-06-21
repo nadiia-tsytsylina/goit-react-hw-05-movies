61b46705e2b02dcfd01d634e0fe3ec1738b9dfa1
Nadiia Tsytsylina <miniova95@gmail.com>
2023-06-21 13:33:19 +0300

    update NotFoundPage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { lazy } from 'react';
 import Layout from './Layout/Layout';
 const Home = lazy(() => import('../pages/Home'));
@@ -6,7 +6,6 @@ const Movies = lazy(() => import('../pages/Movies'));
 const MovieDetails = lazy(() => import('../pages/MovieDetails'));
 const Cast = lazy(() => import('../components/Cast/Cast'));
 const Reviews = lazy(() => import('../components/Reviews/Reviews'));
-const NotFound = lazy(() => import('../components/NotFound/NotFound'));
 
 export const App = () => {
   return (
@@ -19,7 +18,7 @@ export const App = () => {
           <Route path="reviews" element={<Reviews />} />
         </Route>
       </Route>
-      <Route path="*" element={<NotFound />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 };
